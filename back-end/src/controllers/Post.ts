fc69de394b3d6fea27470dbd6cc043cae766f56e
Nick Zimmermann <nicholas.zimmermann@gmail.com>
2020-01-25 02:51:29 -0800

    fixed function that allows us to query for multiple options

--- back-end/src/controllers/Post.ts
+++ back-end/src/controllers/Post.ts
@@ -6,24 +6,26 @@ import { Error } from '../types/Error';
 import { formatGame, formatProjectType } from '../util/textFormat';
 
 //TODO: Figure out how to mend parameters Game and Project Type so that we can reduce duplicate code
-export const getPostsByParamType = async (
-  param: string,
-  page: number
-): Promise<Array<Post> | Error> => {
-  let searchParam;
-  if (formatGame(param) !== 'INVALID_GAME')
-    searchParam = { Game: formatGame(param) };
-  else if (formatProjectType(param) !== 'INVALID_PROJECT_TYPE')
-    searchParam = { ProjectType: formatProjectType(param) };
-  else throw generateError('INVALID_PARAM');
-  console.log(searchParam);
+export const getPostsByQuery = async (query): Promise<Array<Post> | Error> => {
   try {
+    const { page, game, projecttype } = query;
+    if (!page) return generateError('PAGE_REQUIRED');
+    if (!game && !projecttype) return generateError('NO_SPECIFIED_PARAMETERS');
+    let searchParam = {};
+    //figure out what the parameter belongs to
+    if (formatGame(game) !== 'INVALID_GAME')
+      searchParam['Game'] = formatGame(game);
+    if (formatProjectType(projecttype) !== 'INVALID_PROJECT_TYPE')
+      searchParam['ProjectType'] = formatProjectType(projecttype);
+    //then try to find posts related to the parameter
+    console.log(searchParam);
     let post = await Posts.find(searchParam)
       .skip((page - 1) * ITEMS_PER_FETCH)
       .limit(ITEMS_PER_FETCH);
     if (post.length === 0) return generateError('REACHED_BOTTOM');
     return post;
   } catch (err) {
+    console.log(err);
     return generateError('CANNOT_FIND_POSTS');
   }
 };
--- back-end/src/routes/posts/get.ts
+++ back-end/src/routes/posts/get.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import debug from '../../util/debug';
-import { getPostsByParamType, getPostByID } from '../../controllers/Post';
+import { getPostsByQuery, getPostByID } from '../../controllers/Post';
 
 //TODO: Handle Errors
 
@@ -20,10 +20,10 @@ router.get('/id/:id', async (req: express.Request, res: express.Response) => {
 });
 
 //Gets some posts from the specified param
-router.get('/:param', async (req: express.Request, res: express.Response) => {
+router.get('/', async (req: express.Request, res: express.Response) => {
   try {
-    debug(`Retrieving some posts by game ${req.params.param}`);
-    let posts = await getPostsByParamType(req.params.param, req.query.page);
+    debug(`Retrieving some posts by parameters: ${req.query}`);
+    let posts = await getPostsByQuery(req.query);
     res.send(posts);
   } catch (err) {
     res.status(400).send(err);
