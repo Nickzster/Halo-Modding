7e22a4ccf5df9ec8f39fee9c879538367dbfa5b0
Nick Zimmermann <nicholas.zimmermann@gmail.com>
2020-02-08 00:23:21 -0800

    modified back end to allow searches for user names

--- back-end/src/controllers/Post.ts
+++ back-end/src/controllers/Post.ts
@@ -1,14 +1,14 @@
 const ITEMS_PER_FETCH = 5;
-import Posts from '../models/Posts';
-import { Post } from '../types/Post';
-import { generateError } from '../util/generateError';
-import validatePost from '../middleware/validators/ValidatePost';
-import { Query } from '../types/Queries';
-import { Error } from '../types/Error';
-import debug from '../util/debug';
+import Posts from "../models/Posts";
+import { Post } from "../types/Post";
+import { generateError } from "../util/generateError";
+import validatePost from "../middleware/validators/ValidatePost";
+import { Query } from "../types/Queries";
+import { Error } from "../types/Error";
+import debug from "../util/debug";
 
 const stripEmail = (post: Post) => {
-  if (post && post.userinfo && post.userinfo.email) post.userinfo.email = '';
+  if (post && post.userinfo && post.userinfo.email) post.userinfo.email = "";
   return post;
 };
 
@@ -30,32 +30,35 @@ export const getPostsByQuery = async (
     for (let queryType in reqQueries) {
       queries.push({ key: queryType, value: reqQueries[queryType] });
     }
-    console.log('QUERIES', queries);
+    console.log("QUERIES", queries);
     let page: number = parseInt(queries.pop().value, 10);
     let searchParam = {};
-    if (!page) return generateError('PAGE_REQUIRED');
+    if (!page) return generateError("PAGE_REQUIRED");
     if (queries.length > 0)
-      queries.map(query => (searchParam[query.key] = query.value));
+      queries.map(query => {
+        if (query.key === "username")
+          searchParam["userinfo.username"] = query.value;
+        else searchParam[query.key] = query.value;
+      });
     debug(searchParam);
     let post: Array<Post> = await Posts.find(searchParam)
       .sort({ date: -1 })
       .skip((page - 1) * ITEMS_PER_FETCH)
       .limit(ITEMS_PER_FETCH);
-    if (post.length === 0) return generateError('REACHED_BOTTOM');
+    if (post.length === 0) return generateError("REACHED_BOTTOM");
     post.map(p => stripEmail(p));
     return post;
   } catch (err) {
-    debug('# # # THROWN IN getPostsByQuery: ', err);
-    return generateError('CANNOT_FIND_POSTS');
+    debug("# # # THROWN IN getPostsByQuery: ", err);
+    return generateError("CANNOT_FIND_POSTS");
   }
 };
-
 export const getPostByID = async (id: string): Promise<Post | Error> => {
   try {
     let post: Post = await Posts.findOne({ _id: id });
     return stripEmail(post);
   } catch (err) {
     debug(err);
-    return generateError('CANNOT_FIND_POST_BY_ID');
+    return generateError("CANNOT_FIND_POST_BY_ID");
   }
 };
